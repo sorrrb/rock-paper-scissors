4cd037879c4ec0cd5dcc6127b33095432ecf47d4
sorrrb <cdrukepike@gmail.com>
2022-06-06 21:35:30 -0400

    Refactor JavaScript to be descriptive/concise

--- javascript.js
+++ javascript.js
@@ -1,30 +1,7 @@
+const CHOICES = ['Rock', 'Paper', 'Scissors'];
+
 function computerPlay() {
-	const CHOICES = ['Rock', 'Paper', 'Scissors'];
-	const RAND_CHOICE = Math.floor(Math.random() * 3)
-	return CHOICES[RAND_CHOICE];
+	return CHOICES[Math.floor(Math.random() * 3)];
 }
 
-function playRound(playerSelection, computerSelection) {
-	let playerSelectionLower = playerSelection.toLowerCase();
-	let firstCharUpper = playerSelectionLower[0].toUpperCase();
-	let playerSelectionSubstr = playerSelectionLower.slice(1);
-	playerSelection = firstCharUpper + playerSelectionSubstr;
-
-	if (playerSelection === computerSelection) {
-		return "It's a tie!";
-	}
-	else if ((playerSelection === "Rock") && (computerSelection === "Scissors")) {
-		return "You Win! " + playerSelection + " beats " + computerSelection;
-	}
-	else if ((playerSelection === "Paper") && (computerSelection === "Rock")) {
-		return "You Win! " + playerSelection + " beats " + computerSelection;
-	}
-	else if ((playerSelection === "Scissors") && (computerSelection === "Paper")) {
-		return "You Win! " + playerSelection + " beats " + computerSelection;
-	}
-	else {
-		return "You Lose! " + computerSelection + " beats " + playerSelection;
-	}
-}
-
-console.log(playRound(computerPlay(), "Rock"));
+console.log(computerPlay());
