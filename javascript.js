ebc0d99ff0d36de9c3b309807835dea39b3df761
sorrrb <cdrukepike@gmail.com>
2022-06-06 21:11:21 -0400

    Complete skeleton single round code

--- javascript.js
+++ javascript.js
@@ -1,8 +1,30 @@
-const choices = ['Rock', 'Paper', 'Scissors'];
-
 function computerPlay() {
-	let randChoice = Math.floor(Math.random() * 3)
-	return choices[randChoice];
+	const CHOICES = ['Rock', 'Paper', 'Scissors'];
+	const RAND_CHOICE = Math.floor(Math.random() * 3)
+	return CHOICES[RAND_CHOICE];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+	let playerSelectionLower = playerSelection.toLowerCase();
+	let firstCharUpper = playerSelectionLower[0].toUpperCase();
+	let playerSelectionSubstr = playerSelectionLower.slice(1);
+	playerSelection = firstCharUpper + playerSelectionSubstr;
+
+	if (playerSelection === computerSelection) {
+		return "It's a tie!";
+	}
+	else if ((playerSelection === "Rock") && (computerSelection === "Scissors")) {
+		return "You Win! " + playerSelection + " beats " + computerSelection;
+	}
+	else if ((playerSelection === "Paper") && (computerSelection === "Rock")) {
+		return "You Win! " + playerSelection + " beats " + computerSelection;
+	}
+	else if ((playerSelection === "Scissors") && (computerSelection === "Paper")) {
+		return "You Win! " + playerSelection + " beats " + computerSelection;
+	}
+	else {
+		return "You Lose! " + computerSelection + " beats " + playerSelection;
+	}
+}
+
+console.log(playRound(computerPlay(), "Rock"));
