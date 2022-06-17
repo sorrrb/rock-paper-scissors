d26e754657237c46ef4ae868b3031814832f96d1
sorrrb <cdrukepike@gmail.com>
2022-06-17 03:11:28 -0400

    Complete game logic & node targeting
    - Add feature to remove event handlers upon game end
    - Refactor win/lose messages
    - Debug conditional logic on event handlers
    - Add play again button

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 let game = () => {
+  const HACK_CHAR = '&nbsp;';
   let playerWins = 0;
   let computerWins = 0;
   const MOVES = ['Rock', 'Paper', 'Scissors'];
+  const tie = 'tie';
 
   let computerPlay = () => MOVES[Math.round(Math.random() * 2)];
 
@@ -20,19 +22,26 @@ let game = () => {
   let playRound = (playerSelect, computerSelect) => {
     const result = checkWin(playerSelect, computerSelect);
     if (result === null) {
-      return;
+      playerMoveText.textContent = playerSelect;
+      computerMoveText.textContent = computerSelect;
+      scoreText.textContent = defineScore();
+      mainText.textContent = '~ TIE ~';
     }
     else if (result === true) {
       ++playerWins;
       playerMoveText.textContent = playerSelect;
       computerMoveText.textContent = computerSelect;
       scoreText.textContent = defineScore();
+      mainText.textContent = '~ WIN ~';
+      checkEnd();
     }
     else {
       ++computerWins;
       playerMoveText.textContent = playerSelect;
       computerMoveText.textContent = computerSelect;
       scoreText.textContent = defineScore();
+      mainText.textContent = '~ LOSS ~';
+      checkEnd();
     }
   }
 
@@ -40,8 +49,34 @@ let game = () => {
     return playerWins + ' - ' + computerWins;
   }
 
-  const mainText = document.querySelector('p.info--text');
-  const subText = document.querySelector('p.info--subtext');
+  let checkEnd = () => {
+    if (playerWins === 5) {
+      mainText.textContent = 'YOU WIN!';
+      moveBtns.forEach((button) => {
+        button.removeEventListener('click', linkMove);
+      });
+      subText.textContent = 'Play again?';
+      subText.classList.add('replay');
+      subText.addEventListener('click', () => {
+        location.reload();
+      });
+    }
+    if (computerWins === 5) {
+      mainText.textContent = 'YOU LOSE!';
+      moveBtns.forEach((button) => {
+        button.removeEventListener('click', linkMove);
+      });
+      subText.textContent = 'Play again?';
+      subText.classList.add('replay');
+      subText.addEventListener('click', () => {
+        location.reload();
+      });
+    }
+    else return;
+  }
+
+  const mainText = document.querySelector('.info--text');
+  const subText = document.querySelector('.info--subtext');
 
   const logicContainer = document.querySelector('.logic--wrapper');
 
